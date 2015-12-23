edc98979d3d1ad47f15c3a0576dd23cbdfceef28
lanbau <irvinln@gmail.com>
2015-12-23 09:01:32 +0800

    Initial Commit

---
+++ routes/movies.js
@@ -0,0 +1,73 @@
+
+var Movie=require('../models/movie');
+var express=require('express');
+
+//configure routes
+
+var router=express.Router();
+
+router.route('/movies')
+    .get(function(req,res){
+       Movie.find(function(err,movies){
+           if(err)
+                res.send(err);
+           res.json(movies);
+           // res.json({title:'tobi'});
+       });
+    })
+
+    .post(function(req,res){
+        var movie=new Movie(req.body);
+        console.log(req.body)
+        movie.save(function(err){
+            if(err)
+                res.send(err);
+            res.send({message:'Movie Added'});
+        });
+    });
+
+router.route('/movies/:id')
+    .put(function(req,res){
+        Movie.findOne({_id:req.params.id},function(err,movie){
+
+            if(err)
+                res.send(err);
+
+           for(prop in req.body){
+                console.log(req.body[prop])
+                console.log(movie[prop])
+                movie[prop]=req.body[prop];
+           }
+
+            // save the movie
+            movie.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Movie updated!' });
+            });
+
+        });
+    })
+
+    .get(function(req,res){
+        Movie.findOne({_id:req.params.id},function(err, movie) {
+            if(err)
+                res.send(err);
+
+            res.json(movie);
+        });
+    })
+
+    .delete(function(req,res){
+        Movie.remove({
+            _id: req.params.id
+        }, function(err, movie) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+
+module.exports=router;
